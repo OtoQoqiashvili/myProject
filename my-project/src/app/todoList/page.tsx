b6c3c80bedda0796735e907e3c6b2ce0fadd0ac7
OtoQoqiashvili <167248626+OtoQoqiashvili@users.noreply.github.com>
2025-04-07 23:43:45 +0400

    add features

--- my-project/src/app/todoList/page.tsx
+++ my-project/src/app/todoList/page.tsx
@@ -25,39 +25,48 @@ export default function Page() {
 
   return (
     <div className={styles.main_container}>
-    <div className={styles.container}>
-      <h1 className={styles.heading}>my To Do List</h1>
-      <div className={styles.inputGroup}>
-        <input
-          type="text"
-          value={text}
-          onChange={e => setText(e.target.value)}
-          placeholder="Add a task..."
-          className={styles.input}
-        />
-        <button onClick={addTodo} className={styles.addButton}>
-          Add
-        </button>
+      <div className={styles.container}>
+        <h1 className={styles.heading}>my To Do List</h1>
+        <div className={styles.inputGroup}>
+          <input
+            type="text"
+            value={text}
+            onChange={e => setText(e.target.value)}
+            onKeyDown={e => {
+              if (e.key === "Enter") {
+                addTodo();
+              }
+            }}
+            placeholder="Add a task..."
+            className={styles.input}
+          />
+          <button onClick={addTodo} className={styles.addButton}>
+            Add
+          </button>
+        </div>
+        <ul className={styles.todoList}>
+          {todos.map(todo => (
+            <div className={styles.tast_card}>
+              <div 
+              className={`${styles.checkbox} ${todo.completed ? styles.checkbox_completed : ""}`}
+              onClick={() => toggleTodo(todo.id)}/>
+              <li key={todo.id} className={`${styles.todoItem} ${todo.completed ? styles.todoItem_completed : ""}`}>
+                <span
+                  className={`${styles.todoText} ${todo.completed ? styles.completed : ""}`}
+                >
+                  {todo.text}
+                </span>
+                <button
+                  onClick={() => deleteTodo(todo.id)}
+                  className={styles.deleteButton}
+                >
+                  X
+                </button>
+              </li>
+            </div>
+          ))}
+        </ul>
       </div>
-      <ul className={styles.todoList}>
-        {todos.map(todo => (
-          <li key={todo.id} className={`${styles.todoItem} ${todo.completed ? styles.todoItem_completed : ""}`}>
-            <span
-              onClick={() => toggleTodo(todo.id)}
-              className={`${styles.todoText} ${todo.completed ? styles.completed : ""}`}
-            >
-              {todo.text}
-            </span>
-            <button
-              onClick={() => deleteTodo(todo.id)}
-              className={styles.deleteButton}
-            >
-              X
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
     </div>
   );
 }
